0b8aeaa5ee5ffa8b92788271869bcf68a86c3150
doublev2906 <83346733+doublev2906@users.noreply.github.com>
2021-12-03 17:56:43 +0700

    Update EnemyAI.java

--- src/main/java/com/example/bbbbb/enemy/EnemyAI.java
+++ src/main/java/com/example/bbbbb/enemy/EnemyAI.java
@@ -6,56 +6,7 @@ import com.example.bbbbb.Direction;
 import java.util.Random;
 
 public class EnemyAI {
-//    Bomber _bomber;
-//    Enemy _e;
-//    Random random;
-//
-//    public EnemyAI(Bomber bomber, Enemy e) {
-//        _bomber = bomber;
-//        _e = e;
-//        random = new Random();
-//    }
 
-
-//    public int calculateDirection() {
-//        // TODO: cài đặt thuật toán tìm đường đi
-//        if(_bomber == null)
-//            return random.nextInt(4);
-//
-//        int vertical = random.nextInt(2);
-//
-//        if(vertical == 1) {
-//            int v = calculateRowDirection();
-//            if(v != -1)
-//                return v;
-//            else
-//                return calculateColDirection();
-//
-//        } else {
-//            int h = calculateColDirection();
-//
-//            if(h != -1)
-//                return h;
-//            else
-//                return calculateRowDirection();
-//        }
-//    }
-//    protected int calculateColDirection() {
-//        if(_bomber.getX() / 32 < _e.getX()/32)
-//            return 3;
-//        else if(_bomber.getX() / 32 > _e.getX()/32)
-//            return 1;
-//
-//        return -1;
-//    }
-//
-//    protected int calculateRowDirection() {
-//        if(_bomber.getY() < _e.getY())
-//            return 0;
-//        else if(_bomber.getY() > _e.getY())
-//            return 2;
-//        return -1;
-//    }
     Bomber player;
     Enemy enemy;
 
